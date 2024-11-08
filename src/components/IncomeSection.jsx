d9b6cced4f00d8becf689e3fc1bb961458b88f94
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2024-11-08 12:53:23 +0000

    Changed the income input, used onClick and onBlur handler to handle display and input

--- src/components/IncomeSection.jsx
+++ src/components/IncomeSection.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import MainHeading from "./MainHeading";
 import SectionWrapper from "./SectionWrapper";
 import StatsWrapper from "./StatsWrapper";
@@ -9,6 +10,8 @@ export default function IncomeSection({
   annualTakeHome,
   monthlytakeHome,
 }) {
+  const [inputVisible, setInputVisible] = useState(false);
+  const inputref = useRef(null);
   const incomeStats = [
     {
       text: "Annual take home pay:",
@@ -28,6 +31,12 @@ export default function IncomeSection({
     },
   ];
 
+  useEffect(() => {
+    if (inputVisible) {
+      inputref.current.focus();
+    }
+  }, [inputVisible]);
+
   return (
     <SectionWrapper>
       <MainHeading
@@ -42,23 +51,41 @@ export default function IncomeSection({
 
       <label htmlFor="userIncome" className="text-center font-light text-lg">
         Your Annual Income:{" "}
-        <span className="text-3xl pl-2 text-green-400">£</span>
+        <span className="text-3xl pl-1 text-green-400">£</span>
+        {!inputVisible && (
+          <span
+            className="rounded pl-1 w-8/12  max-w-56 bg-green-600 text-3xl text-green-400 bg-opacity-5"
+            onClick={() => {
+              setInputVisible(true);
+            }}
+          >
+            {userIncome.toLocaleString(undefined, {
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 0,
+            })}
+          </span>
+        )}
         <input
+          id="input"
+          ref={inputref}
+          onBlur={() => {
+            setInputVisible(false);
+          }}
           name="userIncome"
-          type="text"
+          type="number"
           min={0}
           placeholder="Enter here"
-          value={
-            userIncome === 0
-              ? ""
-              : userIncome.toLocaleString(undefined, {
-                  minimumFractionDigits: 0,
-                  maximumFractionDigits: 0,
-                })
-          }
-          className="rounded pl-1 w-8/12 max-w-56 bg-green-600 text-3xl text-green-400 bg-opacity-5 focus:bg-green-300 focus:text-green-800"
+          value={userIncome === 0 ? "" : userIncome}
+          className={`rounded pl-1 w-8/12 max-w-32 bg-green-600 text-3xl text-green-400 bg-opacity-5 focus:bg-green-300 focus:text-green-800 ${
+            inputVisible ? `inline` : `hidden`
+          }`}
+          onKeyDown={(event) => {
+            if (event.key == "Enter") {
+              setInputVisible(false);
+            }
+          }}
           onChange={(event) => {
-            let income = event.target.value.replace(/[^0-9.]/g, "");
+            let income = Number(event.target.value.replace(/[^0-9.]/g, ""));
             setUserIncome(income ? parseFloat(income) : 0);
           }}
         />
